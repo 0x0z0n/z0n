f0177e0c6d90cb5a9cfe05d61334e5c4487e6b01
z0n <hhorizon97@gmail.com>
2026-01-17 18:56:32 +0000

    Giveback

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if a commit message was provided
+
 if [ -z "$1" ]
 then
   echo "Error: No commit message provided."
@@ -8,12 +8,6 @@ then
   exit 1
 fi
 
-# 1. Add all changes
 git add .
-
-# 2. Commit with the provided message
 git commit -m "$1"
-
-# 3. Push to the main branch
-# Note: This will prompt for your password/PAT once unless cached
 git push origin main
